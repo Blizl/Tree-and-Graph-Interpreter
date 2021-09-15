1e2d2bd53346723594b37e955154b8edee8ba545
Victor Liang <vliang621@gmail.com>
2021-09-14 18:45:41 -0700

    Add union by rank and path compression

--- UnionFind.java
+++ UnionFind.java
@@ -1,5 +1,5 @@
 public class UnionFind {
-
+    int[] ranks;
     int[] parent;
     int m, n;
     int numOfComponents;
@@ -13,6 +13,7 @@ public class UnionFind {
                 if (grid[i][j] == '1') {
                     int id = i * n +j;
                     parent[id] = id;
+                    ranks[id] = 1;
                     numOfComponents++;
                 }
 
@@ -33,6 +34,24 @@ public class UnionFind {
 
     }
 
+    public void unionWithRank(int node1, int node2) {
+        int root1 = find(node1);
+        int root2 = find(node2);
+        if (root1 != root2) {
+            if (ranks[root1] > ranks[root2]) {
+                parent[root2] = root1;
+
+            } else if (ranks[root1] < ranks[root2]) {
+                parent[root1] = root2;
+            } else {
+                // Doesn't matter which we set the parent to, just need to make sure to increase the rank
+                parent[root2] = root1;
+                ranks[root1]++;
+            }
+            numOfComponents--;
+        }   
+    }
+
     public int find(int node) {
         if (parent[node] == node) {
             return node;
@@ -42,5 +61,15 @@ public class UnionFind {
         }
         return node;
     }
+
+    public int findWithPathCompression(int node) {
+        int oldId = node;
+        while (parent[node] != node) {
+            node = parent[node];
+        }
+        parent[oldId] = node;
+        return node;
+    }
+
 
 }
